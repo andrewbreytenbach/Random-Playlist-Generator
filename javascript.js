0a27af3995fa3bf5a7af04f98907260189c54ae4
Emily Miles <emilymiles193@gmail.com>
2023-04-30 00:51:31 -0400

    started javascript

---
+++ javascript.js
@@ -0,0 +1,4 @@
+let dropDownMenu= document.getElementById("sub-menu");
+dropDownMenu.addEventListener("click", function(e) {
+    document.getElementById("hide").setAttribute("id", "visible");
+  });
