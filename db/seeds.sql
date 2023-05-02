8363f3b5694a439104889fd164ac9b72266b8d70
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-05-02 18:56:21 -0400

    added to the seeds.db file

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,3 +8,17 @@ INSERT INTO songs (title, artist, year, genre, length) VALUES ('Smells Like Teen
 INSERT INTO songs (title, artist, year, genre, length) VALUES ('Hallelujah', 'Jeff Buckley', 1994, 'Rock', 307);
 INSERT INTO songs (title, artist, year, genre, length) VALUES ('Lose Yourself', 'Eminem', 2002, 'Hip hop', 337);
 INSERT INTO songs (title, artist, year, genre, length) VALUES ('Hotel California', 'Eagles', 1976, 'Rock', 391);
+INSERT INTO songs (title, artist, year, genre, length) VALUES ('Despacito', 'Luis Fonsi ft. Daddy Yankee', 2017, 'Reggaeton', 228);
+INSERT INTO songs (title, artist, year, genre, length) VALUES ('Sorry', 'Justin Bieber', 2015, 'Pop', 200);
+INSERT INTO songs (title, artist, year, genre, length) VALUES ('Hello', 'Adele', 2015, 'Pop', 295);
+INSERT INTO songs (title, artist, year, genre, length) VALUES ('Uptown Funk', 'Mark Ronson ft. Bruno Mars', 2014, 'Funk', 270);
+INSERT INTO songs (title, artist, year, genre, length) VALUES ('We Found Love', 'Rihanna ft. Calvin Harris', 2011, 'Pop', 219);
+INSERT INTO songs (title, artist, year, genre, length) VALUES ('Purple Haze', 'Jimi Hendrix', 1967, 'Rock', 166);
+INSERT INTO songs (title, artist, year, genre, length) VALUES ('Imagine', 'John Lennon', 1971, 'Rock', 185);
+INSERT INTO songs (title, artist, year, genre, length) VALUES ('Sweet Child O Mine', 'Guns N Roses', 1987, 'Rock', 356);
+INSERT INTO songs (title, artist, year, genre, length) VALUES ('I Will Always Love You', 'Whitney Houston', 1992, 'Pop', 271);
+INSERT INTO songs (title, artist, year, genre, length) VALUES ('Nothing Compares 2 U', 'Sinéad OConnor', 1990, 'Pop', 301);
+INSERT INTO songs (title, artist, year, genre, length) VALUES ('Billie Jean', 'Michael Jackson', 1983, 'Pop', 294);
+INSERT INTO songs (title, artist, year, genre, length) VALUES ('Born to Run', 'Bruce Springsteen', 1975, 'Rock', 294);
+INSERT INTO songs (title, artist, year, genre, length) VALUES ('With or Without You', 'U2', 1987, 'Rock', 296);
+INSERT INTO songs (title, artist, year, genre, length) VALUES ('Beat It', 'Michael Jackson', 1982, 'Pop', 258);
