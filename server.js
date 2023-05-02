23ef32ff5a13b06c900dcc3df07dce9d1b5663ed
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-05-02 15:04:50 -0400

    updated the files

--- server.js
+++ server.js
@@ -1,37 +1,46 @@
-// Require necessary modules
-const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
-const db = require('./models');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
-// Set up Express app
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-// Set up middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Set up HTML routes
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+app.use(routes);
 
-// Set up API routes
-app.get('/api/songs', (req, res) => {
-    db.Song.findAll({}).then((dbSongs) => {
-        res.json(dbSongs);
-    });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
-
-// Start the server
-db.sequelize.sync({ force: true })
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server listening on PORT ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error('Error synchronizing Sequelize models with database:', err);
-  });
