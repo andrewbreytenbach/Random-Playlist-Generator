e07858f745dcd41b140c80610df09ce7253d1b7c
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-05-02 20:58:05 -0400

    launched the server

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,10 @@
-const sequelize = require('./config/connection');
-const { Song } = require('./models');
+const sequelize = require('../config/connection');
+const { Song } = require('../models');
 
 const songData = require('./songData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
-
-  await Song.bulkCreate(songData);
 
   process.exit(0);
 };
