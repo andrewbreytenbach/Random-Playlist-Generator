f91ef63cc763c8aac6b26209b77fa1477cdc06ec
Kriti Shrestha <Kriti@Sarojs-MacBook-Pro.local>
2023-05-03 23:24:30 -0400

    merging to main

--- server.js
+++ server.js
@@ -1,67 +1,9 @@
-// // Require necessary modules
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const path = require('path');
-// const db = require('./models');
-
-// //setting up handlebars
-// const exphbs = require('express-handlebars');
-// const hbs = exphbs.create({});
-
-// // Set up Express app
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// // Set up middleware
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // Set up HTML routes
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public/index.html'));
-// });
-
-// //defining routes
-// const loginRoutes = require('./api/loginRoutes');
-// const signupRoutes = require('./api/signupRoutes');
-
-// app.use('/', loginRoutes);
-// app.use('/', signupRoutes);
-
-// //Render auth template that consists of login and signup
-// app.get('/auth', (req, res) => {
-//   res.render('auth');
-// });
-
-// //set up default engine
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-
-// // Set up API routes
-// app.get('/api/songs', (req, res) => {
-//   // Placeholder response
-//   const songs = [
-//     { id: 1, title: 'Song 1', artist: 'Artist 1', year: 2000, genre: 'Rock' },
-//     { id: 2, title: 'Song 2', artist: 'Artist 2', year: 2005, genre: 'Pop' },
-//     { id: 3, title: 'Song 3', artist: 'Artist 3', year: 2010, genre: 'Hip-hop' },
-//   ];
-//   res.json(songs);
-// });
-
-// // Start the server
-// db.sequelize.sync().then(() => {
-//   app.listen(PORT, () => {
-//     console.log(`Server listening on PORT ${PORT}`);
-//   });
-// });
-
-
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -70,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create();
+const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
@@ -87,18 +29,45 @@ const sess = {
   })
 };
 
+
+// Set up session middleware
 app.use(session(sess));
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// Set up static file serving and body parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set up routes
 app.use(routes);
 
+// Start the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`);
+  });
+});
+
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Set up static file serving and body parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up routes
+app.use(routes);
+
+// Start the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`);
+  });
 });
