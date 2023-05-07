ecee2281c96d2ebf4b512ab23b9cc3c4d117df36
Star Child <tajagrayson@Tajas-MacBook-Air.local>
2023-05-07 19:19:29 -0400

    added decade search

--- public/js/search.js
+++ public/js/search.js
@@ -1,5 +1,6 @@
 const artistBtn = document.getElementById("artist")
 const genreBtn = document.getElementById("genre")
+const DecadeBtn = document.getElementById("decade")
 
 function searchByArtist () {
     const artistInput = document.getElementById("input-one").value
@@ -31,10 +32,10 @@ function searchByArtist () {
             const genreEl = document.createElement("h6");
             genreEl.textContent = data[i].genre;
             
-            const yearEl = document.createElement("h6");
-            yearEl.textContent = data[i].year;
+            const decadeEl = document.createElement("h6");
+            decadeEl.textContent = data[i].decade;
 
-            newLi.append(titleEl, artistEl, genreEl, yearEl)
+            newLi.append(titleEl, artistEl, genreEl, decadeEl)
 
             resultsEl.append(newLi)
         }
@@ -64,16 +65,50 @@ function searchByGenre () {
                 const genreEl = document.createElement("h6");
                 genreEl.textContent = data[i].genre;
                 
-                const yearEl = document.createElement("h6");
-                yearEl.textContent = data[i].year;
+                const decadeEl = document.createElement("h6");
+                decadeEl.textContent = data[i].decade;
     
-                newLi.append(titleEl, artistEl, genreEl, yearEl)
+                newLi.append(titleEl, artistEl, genreEl, decadeEl)
     
                 resultsEl.append(newLi)
             }
         })
     }
 
+    function searchByDecade () {
+        const decadeInput = document.getElementById("input-three").value
+        fetch("/api/songs/decade/" + decadeInput)
+        .then(res => res.json())
+        .then(data => {
+                console.log(data)
+                
+                const resultsEl = document.getElementById("results");
+    
+                resultsEl.innerHTML = "";
+        
+                for(i = 0; i < data.length; i++) {
+                    const newLi = document.createElement("li");
+        
+                    const titleEl = document.createElement("h4");
+                    titleEl.textContent = data[i].title;
+        
+                    const artistEl = document.createElement("h5");
+                    artistEl.textContent = data[i].artist;
+        
+                    const genreEl = document.createElement("h6");
+                    genreEl.textContent = data[i].genre;
+                    
+                    const decadeEl = document.createElement("h6");
+                    decadeEl.textContent = data[i].decade;
+        
+                    newLi.append(titleEl, artistEl, genreEl, decadeEl)
+        
+                    resultsEl.append(newLi)
+                }
+            })
+        }
+
 
 artistBtn.addEventListener("click", searchByArtist);
 genreBtn.addEventListener("click", searchByGenre);
+yearBtn.addEventListener("click", searchByDecade)
